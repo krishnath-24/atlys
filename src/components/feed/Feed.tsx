904c68baeafc3fc3007e9e8eb2e5b51bd70a3b82
Anuj Sharma <sharma.anuj2498@gmail.com>
2025-07-04 14:55:36 +0530

    login trigger on interaction

--- src/components/feed/Feed.tsx
+++ src/components/feed/Feed.tsx
@@ -33,7 +33,7 @@ export default function Feed() {
   }, [isLoggedIn]);
 
   return (
-    <div className="max-w-2xl mx-auto px-4 py-6" aria-live='polite'>
+    <div className="max-w-2xl mx-auto px-4 py-6" aria-live='polite' onClick={checkIfLoggedIn}>
       <PostComposer onClick={checkIfLoggedIn} featureNotImplemented={featureNotImplemented} />
       <AnimatePresence initial={false}>
         {
